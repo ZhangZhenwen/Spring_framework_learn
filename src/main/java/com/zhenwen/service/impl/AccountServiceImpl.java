cf43a3981cf8baaec70b84651bd9d7a69e090cbd
ZhangZhenwen <13075808821@163.com>
2020-03-26 19:42:00 +0800

    day03 account proxy

--- src/main/java/com/zhenwen/service/impl/AccountServiceImpl.java
+++ src/main/java/com/zhenwen/service/impl/AccountServiceImpl.java
@@ -3,13 +3,12 @@ package com.zhenwen.service.impl;
 import com.zhenwen.dao.AccountDao;
 import com.zhenwen.entity.Account;
 import com.zhenwen.service.AccountService;
-import com.zhenwen.utils.TransactionManager;
 
 import java.util.List;
 
 /**
  * created by IntelliJ IDEA
-
+ *
  * @author zhenwen
  * @version jdk 11
  * @date 2020/3/10
@@ -18,16 +17,9 @@ import java.util.List;
 public class AccountServiceImpl implements AccountService {
 
     private AccountDao accountDao;
-    private TransactionManager manager;
-
-
 
     public void setAccountDao(AccountDao accountDao) {
         this.accountDao = accountDao;
-    }
-
-    public void setManager(TransactionManager manager) {
-        this.manager = manager;
     }
 
     @Override
@@ -57,23 +49,11 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void transfer(String sourceName, String targetName, Float salary) {
-        try {
-            manager.beginTransaction();
-
-            Account source = accountDao.findAccountByName(sourceName);
-            Account target = accountDao.findAccountByName(targetName);
-            source.setSalary(source.getSalary() - salary);
-            target.setSalary(target.getSalary() + salary);
-            accountDao.updateAccount(source);
-            accountDao.updateAccount(target);
-
-            manager.commit();
-        } catch (Exception e) {
-            manager.rollback();
-            e.printStackTrace();
-        } finally {
-            manager.release();
-        }
-
+        Account source = accountDao.findAccountByName(sourceName);
+        Account target = accountDao.findAccountByName(targetName);
+        source.setSalary(source.getSalary() - salary);
+        target.setSalary(target.getSalary() + salary);
+        accountDao.updateAccount(source);
+        accountDao.updateAccount(target);
     }
 }
